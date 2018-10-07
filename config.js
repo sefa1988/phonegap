1e4023085c86b9b38c5e271b131474e798536818
sefa1988 <43920426+sefa1988@users.noreply.github.com>
2018-10-07 05:36:39 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'yatirim-kredi',
+		wp_ws_url : 'https://www.yatirimkredi.com/wp-appkit-api/yatirim-kredi',
+		wp_url : 'https://www.yatirimkredi.com',
+		theme : 'q-android',
+		version : '1.0',
+		app_type : 'phonegap-build',
+		app_title : 'Yatırım Kredi',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'F/NzU I;[hU9@1}p;0q]CL2_/?]|OicR:NE_46(YJ6^rfy#mNZJ{UKg7z`)k<mjP',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
